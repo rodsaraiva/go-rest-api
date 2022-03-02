1dd7c095da36340b3b09f021afe898322b4eaf79
Rodrigo Saraiva <rassaraiva@gmail.com>
2022-03-02 16:39:54 -0300

    Implementing Zap Logger

--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -2,11 +2,11 @@ package domain
 
 import (
 	"database/sql"
-	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/rodsaraiva/go-rest-api/errs"
+	"github.com/rodsaraiva/go-rest-api/logger"
 )
 
 type CustomerRepositoryDb struct {
@@ -20,7 +20,7 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 	rows, err := d.client.Query(findAllSql)
 
 	if err != nil {
-		log.Println("Error while querying customer table " + err.Error())
+		logger.Error("Error while querying customer table " + err.Error())
 		return nil, err
 	}
 
@@ -29,7 +29,7 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 		var c Customer
 		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
 		if err != nil {
-			log.Println("Error while scanning customers " + err.Error())
+			logger.Error("Error while scanning customers " + err.Error())
 			return nil, err
 		}
 		customers = append(customers, c)
@@ -48,7 +48,7 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
 		} else {
-			log.Println("Error while scanning customer " + err.Error())
+			logger.Error("Error while scanning customer " + err.Error())
 			return nil, errs.NewUnecpectedError("Unexpected database error")
 		}
 	}
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/rodsaraiva/go-rest-api/app"
+import (
+	"github.com/rodsaraiva/go-rest-api/app"
+	"github.com/rodsaraiva/go-rest-api/logger"
+)
 
 func main() {
 
+	logger.Info("Starting the application...")
 	app.Start()
 
 }
